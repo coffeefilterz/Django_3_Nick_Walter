ef2d69523199f552d1cf9f03043a49b232a2db0c
Anders Lindell Heggø <andheg@globalconnect.no>
2021-04-09 09:41:12 +0200

    Changes after adding git

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/andheg/training/udemy/Django_3_Nick_Walter/django3-personal-portfolio/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
